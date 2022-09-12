cf9237357fb4f7e1cbdc59321bf808a4c861e07c
Naoto Hamada <naoto.hamada@findy.co.jp>
2022-09-13 00:53:58 +0900

    Add avatars#create

--- app/controllers/avatars_controller.rb
+++ app/controllers/avatars_controller.rb
@@ -1,2 +1,5 @@
 class AvatarsController < ApplicationController
+  def create
+    render action: :index
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :avatars, only: :index
+  resources :avatars, only: %i[index create]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
